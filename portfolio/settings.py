13bbeec10818056f84ce61e54065b57cba77ffe4
kartik039 <kartikjoshi039@gmail.com>
2019-08-23 23:00:44 +0530

    added bootstrap

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['jobs/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
